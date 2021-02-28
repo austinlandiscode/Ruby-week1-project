75cb86cccb44687bb92b1d9696a323602815118b
Austin Landis <austinlandiscode@gmail.com>
2021-02-28 14:26:36 -0800

    work on rewriting logic

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,13 +1,1 @@
-class AnagramSolver
-  string1 = "horse"
-  string2 = "shore"
-  def anagram_checker(string1, string2)
-    @string_one = string1.chars.sort.join()
-    @string_two = string2.chars.sort.join()
-  end
-    if @string_one == @string_two
-      true
-    else
-      false
-    end
-end
+
